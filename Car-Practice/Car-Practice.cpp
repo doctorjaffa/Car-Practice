1b323672fc06ddcd84f6ce4dca6d74b45f761d1e
doctorjaffa <79138930+doctorjaffa@users.noreply.github.com>
2022-10-04 16:20:13 +0100

    Added vector and array of car objects.

--- Car-Practice/Car-Practice.cpp
+++ Car-Practice/Car-Practice.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
 #include "Car.h"
 
 int main()
@@ -10,10 +12,13 @@ int main()
     Car car1("Average", "Words", 4, 45.0f);
     Car car2("Speed", "Fancy", 2, 30.0f);
     Car car3("Forb", "Family", 6, 50.0f);
+    Car car4;
 
+    /*
     car1.Print();
     car2.Print();
     car3.Print();
+    
 
     float miles;
     std::cout << "Please enter the miles driven: ";
@@ -23,7 +28,30 @@ int main()
 
     car1.Print();
 
-    Car car4;
     car4.Print();
+    */
 
+    std::vector<Car> carVector;
+    
+    carVector.push_back(car1);
+    carVector.push_back(car2);
+    carVector.push_back(car3);
+    carVector.push_back(car4);
+
+    for (int i = 0; i < carVector.size(); ++i)
+    {
+        carVector[i].Print();
+    }
+
+    Car carArray[4];
+
+    carArray[0] = car1;
+    carArray[1] = car2;
+    carArray[2] = car3;
+    carArray[3] = car4;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        carArray[i].Print();
+    }
 }
